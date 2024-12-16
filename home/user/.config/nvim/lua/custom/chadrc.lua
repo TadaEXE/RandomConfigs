4a9aee8184b042b00a19e7a695e2c898655e27d3
TadaEXE <nevioxbl@gmail.com>
2024-12-16 22:14:28 +0100

    Activate relative numbers by default

--- home/user/.config/nvim/lua/custom/chadrc.lua
+++ home/user/.config/nvim/lua/custom/chadrc.lua
@@ -1,22 +1,10 @@
 ---@type ChadrcConfig 
 
--- vim.keymap.set('n', '<leader>1', '<cmd>tabn 1<cr>')
--- vim.keymap.set('n', '<leader>2', '<cmd>tabn 2<cr>')
--- vim.keymap.set('n', '<leader>3', '<cmd>tabn 3<cr>')
--- vim.keymap.set('n', '<leader>4', '<cmd>tabn 4<cr>')
--- vim.keymap.set('n', '<leader>5', '<cmd>tabn 5<cr>')
--- vim.keymap.set('n', '<leader>6', '<cmd>tabn 6<cr>')
--- vim.keymap.set('n', '<leader>7', '<cmd>tabn 7<cr>')
--- vim.keymap.set('n', '<leader>8', '<cmd>tabn 8<cr>')
--- vim.keymap.set('n', '<leader>9', '<cmd>tabn 9<cr>')
--- vim.keymap.set('n', '<leader>-', '<cmd>tabnext<cr>')
--- vim.keymap.set('n', '<leader>=', '<cmd>tabprevious<cr>')
--- vim.keymap.set('n', '<leader>`', '<cmd>tabnew<cr>')
--- vim.keymap.set('n', '<leader><BS>', '<cmd>tabclose<cr>')
---
 vim.keymap.set({'n', 'i', 'v'}, '<C-s>', '<cmd>w<cr>')
 vim.keymap.set({'i'}, '<C-z>', '<cmd>undo<cr>')
 vim.keymap.set({'i'}, '<C-y>', '<cmd>redo<cr>')
+
+vim.opt.relativenumber = true
 
  local M = {}
  M.ui = { theme = 'oxocarbon' }
